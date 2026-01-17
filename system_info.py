f0e3fcd0e7ab902dc7da3b6c81b0192dd8f05aaf
nattapongpailom <nattapongpailom02112550@gmail.com>
2026-01-17 22:44:20 +0700

    this is how to know you OS, CPU

---
+++ system_info.py
@@ -0,0 +1,14 @@
+import platform
+
+platform.system()
+platform.processor()
+
+My_platform = platform.system()
+My_CPU = platform.processor()
+
+print(f"My Os is:{My_platform}")
+print(f"My CPU is:{My_CPU}")
+
+
+
+
